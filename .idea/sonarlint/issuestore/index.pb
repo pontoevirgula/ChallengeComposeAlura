46f528bfe2b76596e2d7cf63ecba560d3c3eb102
carloslima <carloslima@brq.com>
2022-09-17 13:35:00 -0300

    customizando tela de perfil

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,7 @@
 
 y
-Iapp/src/main/java/com/chslcompany/challengecomposealura/ui/theme/Color.kt,5\d\5de979bc634817f245ff8757b6d6920d4f12d6a1
-y
 Iapp/src/main/java/com/chslcompany/challengecomposealura/ui/theme/Shape.kt,b\b\bb1863d62b34ed6309d1c7ad89b5c2a25fd1220a
 P
  app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- app/src/main/java/com/chslcompany/challengecomposealura/MainActivity.kt
+++ app/src/main/java/com/chslcompany/challengecomposealura/MainActivity.kt
@@ -4,17 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.chslcompany.challengecomposealura.ui.theme.ChallengeComposeAluraTheme
+import com.chslcompany.challengecomposealura.ui.theme.Purple200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,15 +45,63 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ProfileScreen() {
     Column {
-        Row {
+        val boxHeight = remember {
+            150.dp
+        }
+        val imageHeight = remember {
+            boxHeight
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = Color(0xFF2d333b),
+                    shape = RoundedCornerShape(
+                        bottomStart = 16.dp,
+                        bottomEnd = 16.dp
+                    )
+                )
+                .height(boxHeight)
+        ) {
             Image(
                 painter = painterResource(id = R.drawable.profile_picture),
-                contentDescription = "profile pic"
+                contentDescription = "profile pic",
+                modifier = Modifier
+                    .offset(y = imageHeight / 2)
+                    .size(imageHeight)
+                    .align(Alignment.BottomCenter)
+                    .clip(CircleShape)
+            )
+
+        }
+        Spacer(modifier = Modifier.height(imageHeight / 2))
+        Column(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Carlos Henrique",
+                fontSize = 32.sp
+            )
+            Text(
+                "pontoevirgula",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
             )
         }
-        Text(text = "Carlos Henrique")
-        Text(text = "pontoevirgula")
-        Text(text = "Android Software Developer at BRQ")
+        Text(
+            "Android Software Developer at BRQ",
+            Modifier
+                .padding(
+                    start = 8.dp,
+                    bottom = 8.dp,
+                    end = 8.dp
+                )
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
     }
 }
 
