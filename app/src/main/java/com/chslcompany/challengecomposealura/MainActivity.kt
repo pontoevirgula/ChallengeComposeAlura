9756888cb17a4283cafaa4c20efb53d38970311e
carloslima <carloslima@brq.com>
2022-09-20 06:21:22 -0300

    add lib de carregaar imagem assincrona

--- app/src/main/java/com/chslcompany/challengecomposealura/MainActivity.kt
+++ app/src/main/java/com/chslcompany/challengecomposealura/MainActivity.kt
@@ -3,11 +3,11 @@ package com.chslcompany.challengecomposealura
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -17,14 +17,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import coil.request.ImageRequest
 import com.chslcompany.challengecomposealura.ui.theme.ChallengeComposeAluraTheme
-import com.chslcompany.challengecomposealura.ui.theme.Purple200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,16 +67,25 @@ fun ProfileScreen() {
                 )
                 .height(boxHeight)
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.profile_picture),
-                contentDescription = "profile pic",
+            SubcomposeAsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data( "https://avatars.githubusercontent.com/u/13897464?v=4")
+                    .crossfade(true)
+                    .build(),
+                contentDescription = stringResource(R.string.app_name),
                 modifier = Modifier
                     .offset(y = imageHeight / 2)
                     .size(imageHeight)
                     .align(Alignment.BottomCenter)
                     .clip(CircleShape)
-            )
-
+            ) {
+                val state = painter.state
+                if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                    CircularProgressIndicator()
+                } else {
+                    SubcomposeAsyncImageContent()
+                }
+            }
         }
         Spacer(modifier = Modifier.height(imageHeight / 2))
         Column(
